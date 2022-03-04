767b3d4792310c2650a481373dabc003c60c2788
leviterante <terantelevi@gmail.com>
2022-03-04 21:30:04 +0100

    push

---
+++ travel_advisor/src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import { CssBaseline, Grid } from '@material-ui/core'
+
+import { getPlacesData } from './api'
+import { Header, Map, List } from './components'
+
+const App = () => {
+  const [places, setPlaces] = useState([])
+
+  const [coordinates, setCoordinates] = useState({})
+  const [bounds, setBounds] = useState({})
+
+  useEffect(() => {
+    getPlacesData().then((data) => {
+      setPlaces(data)
+    })
+  }, [])
+
+  return (
+    <>
+      <CssBaseline/>
+      <Header/>
+      <Grid container spacing={3} style={{width: '100%'}}>
+        <Grid item xs={12} md={4}>
+          <List/>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+export default App
---
+++ travel_advisor/src/components/index.js
@@ -0,0 +1,11 @@
+import Header from './Header/Header';
+import Map from './Map/Map';
+import List from './List/List';
+import PlaceDetails from './PlaceDetails/PlaceDetails';
+
+export {
+    Header,
+    Map,
+    List,
+    PlaceDetails,
+};
